a073b88c130f04e2ee46404da06f7790c2e30dbc
Yoshihiro-Kitayama <gidai0726@gmail.com>
2024-10-29 22:27:11 +0900

    Sixth-Commit

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -11,41 +11,6 @@ use Illuminate\Support\Facades\Log;
 
 class SalesController extends Controller
 {
-//     public function purchase(Request $request)
-// {
-//     // リクエストから必要なデータを取得する
-//     $productId = $request->input('product_id');
-//     $quantity = $request->input('quantity', 1);
-
-//     // データベースから対象の商品を検索・取得
-//     $product = Product::find($productId);
-
-//     // 商品が存在しない、または在庫が不足している場合のバリデーションを行う
-//     if (!$product) {
-//         return response()->json(['message' => '商品が存在しません'], 404);
-//     }
-//     if ($product->stock < $quantity) {
-//         return response()->json(['message' => '商品が在庫不足です'], 400);
-//     }
-
-//     // 在庫を減少させる
-//     $product->stock -= $quantity;
-//     $product->save();
-
-
-//     // Salesテーブルに商品IDと購入日時を記録する
-//     $sale = new Sale([
-//         'product_id' => $productId,
-
-//     ]);
-
-//     $sale->save();
-
-//     // レスポンスを返す
-//     return response()->json(['message' => '購入成功']);
-// }
-
-
     public function purchase(SaleRequest $request)
     {
 
